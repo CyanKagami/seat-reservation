45f405b9499598d3c58f288ec0758ab1cedc20b9
CyanKagami <boonsitapoopeko@gmail.com>
2026-07-27 14:54:45 +0700

    Add NavBar component

---
+++ src/routes/(client)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+  import NavBar from '$lib/components/NavBar.svelte';
+
+	let { children } = $props();
+</script>
+<div class="flex flex-col justify-center w-full font-inter">
+<NavBar></NavBar>
+	{@render children()}
+</div>
