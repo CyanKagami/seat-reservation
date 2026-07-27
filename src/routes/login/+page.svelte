037c133e9a46ba3dc8a4164e2b61674b49eeaae9
CyanKagami <boonsitapoopeko@gmail.com>
2026-07-27 14:23:41 +0700

    add color pallete and add login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,27 @@
-<div class="self-center justify-self-center flex items-center flex-col">
-    <h1 class="text-4xl font-bold">Login</h1>
-    <button class="rounded-xl drop-shadow-xl w-52 h-20 bg-white mt-5 cursor-pointer">Google o-auth something</button>
-    <button class="rounded-xl drop-shadow-xl w-52 h-15 bg-pink-400 mt-5 cursor-pointer">Login</button>
+<div class="w-full bg-[url(/image/placeholder.png)] h-screen bg-cover">
+    <div class="self-center justify-self-center flex items-center flex-col mt-14">
+  <div class="w-fit flex flex-col items-center">
+    <h1 class="text-7xl font-semibold">
+      K-<span class="text-accent">SEAT</span>
+    </h1>
+    <p class="font-medium">ระบบจองที่นั่งและลงทะเบียนเข้าร่วมกิจกรรม</p>
+  </div>
+  <div class="flex flex-col items-center mt-14 gap-3 bg-white/80 w-fit py-8 rounded-xl px-25 h-fit">
+    <h2 class="font-semibold text-xl">Authentication via email or google authentication</h2>
+    <div class="w-md flex flex-col gap-3">
+      <p class="text-sm font-semibold">EMAIL ADDRESS ( @kmitl.ac.th )</p>
+      <input class="rounded-lg border-gray-400" />
+      <p class="text-sm font-semibold">PASSWORD</p>
+      <input class="rounded-lg border-gray-400" />
+      <button class="bg-accent w-full h-10 rounded-lg text-white font-semibold">LOG IN</button>
+      <div class="flex items-center my-4">
+        <div class="grow border-t border-black"></div>
+        <span class="px-4 text-sm font-medium text-black">OR</span>
+        <div class="grow border-t border-black"></div>
+      </div>
+      <button class="bg-secondary w-full h-10 rounded-lg text-white font-semibold">GOOGLE AUTH</button>
+    </div>
+  </div>
+</div>
+
 </div>
